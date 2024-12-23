6a3c367327d978ba2b4b387418b57bdfbe357a9c
DevalVagadiya <devalvagadiya2005@gmail.com>
2024-12-23 13:47:29 +0530

    Update Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -13,37 +13,37 @@ public class Laptop
 {
 
       @Id
-private int lid;
-private String Iname;
-//@ManyToOne
-//privat Student student;
-@ManyToMany
-private List<Student> student = new ArrayList<Student>();
-
-public int getLid() {
-return lid;
-}
-public void setLid(int lid)
-this.lid lid;
-} 
-public String getLname() {
-return Iname;
-}
-public void setLname(String name) {
-this. Iname Iname;
-}
-/*public Student getStudent() {
-return student;
-}
-
-public Void setStudent(Student student) {
-this.student = student;
-}*/
-public List<Student> getStudent() {
-return student;
-}
-
-public Void setStudent(List<Student> student) {
-this.student = student;
-}
+      private int lid;
+      private String Iname;
+      //@ManyToOne
+      //privat Student student;
+      @ManyToMany
+      private List<Student> student = new ArrayList<Student>();
+      
+      public int getLid() {
+            return lid;
+      }
+      public void setLid(int lid)
+            this.lid = lid;
+      } 
+      public String getLname() {
+            return Iname;
+      }
+      public void setLname(String name) {
+            this. Iname = Iname;
+      }
+      /*public Student getStudent() {
+            return student;
+      }
+      
+      public Void setStudent(Student student) {
+            this.student = student;
+      }*/
+      public List<Student> getStudent() {
+            return student;
+      }
+      
+      public Void setStudent(List<Student> student) {
+            this.student = student;
+      }
 }
