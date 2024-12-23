5d6524a47fd0d616382e3374f6811c72f30318e1
DevalVagadiya <devalvagadiya2005@gmail.com>
2024-12-23 13:41:21 +0530

    Update App_MR.java

--- App_MR.java
+++ App_MR.java
@@ -8,26 +8,28 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 public class App
 {
-public static void main(String args) {
+	    public static void main(String args) {
 
-Laptop laptop = new Laptop();
-laptop.setLid(101);
-laptop.setLname("Dell"); 
+      Laptop laptop = new Laptop();
+      laptop.setLid(101);
+      laptop.setLname("Dell"); 
+      
+      Student s = new Student();
+      s.setName("Navin");
+      s.setRollno(1);
+      s.setMarks(50);
+      //s.setLaptop(Laptop);
+      s.getLaptop().add(laptop);
+      
+      laptop.getStudent().add(s);
 
-Student s = new Student();
-s.setName("Navin");
-s.setRollno(1);
-s.setMarks(50);
-//s.setLaptop(Laptop);
-s.getLaptop().add(laptop);
 
-laptop.getStudent().add(s);
-
-
-Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
-ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
-SessionFactory sf = con.buildSessionFactory(reg);
-Session session = sf.openSession();
+      Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+      ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+      SessionFactory sf = con.buildSessionFactory(reg);
+      Session session = sf.openSession();
+      }
+}
 
 session.beginTransaction();
 
