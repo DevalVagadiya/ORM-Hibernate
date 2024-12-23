32303be533caad5fc39cbfc6b0ea851937b9ad20
DevalVagadiya <devalvagadiya2005@gmail.com>
2024-12-23 13:44:06 +0530

    Update App_MR.java

--- App_MR.java
+++ App_MR.java
@@ -8,34 +8,32 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 public class App
 {
-	    public static void main(String args) {
+	public static void main(String args) {
 
-      Laptop laptop = new Laptop();
-      laptop.setLid(101);
-      laptop.setLname("Dell"); 
-      
-      Student s = new Student();
-      s.setName("Navin");
-      s.setRollno(1);
-      s.setMarks(50);
-      //s.setLaptop(Laptop);
-      s.getLaptop().add(laptop);
-      
-      laptop.getStudent().add(s);
-
-
-      Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
-      ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
-      SessionFactory sf = con.buildSessionFactory(reg);
-      Session session = sf.openSession();
-      }
-}
-
-session.beginTransaction();
-
-session.save(laptop);
-session.save(s);
-
-session.getTransaction().commit();
-}
+	Laptop laptop = new Laptop();
+	laptop.setLid(101);
+	laptop.setLname("Dell"); 
+	      
+	Student s = new Student();
+	s.setName("Navin");
+	s.setRollno(1);
+	s.setMarks(50);
+	/s.setLaptop(Laptop);
+	s.getLaptop().add(laptop);
+	      
+	laptop.getStudent().add(s);
+	
+	
+	Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+	SessionFactory sf = con.buildSessionFactory(reg);
+	Session session = sf.openSession();
+	
+	session.beginTransaction();
+	
+	session.save(laptop);
+	session.save(s);
+	
+	session.getTransaction().commit();
+	}
 }
